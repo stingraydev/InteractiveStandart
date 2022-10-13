f24897e728b91608ee0f7a3d1dc0a72d29a95e4a
stingraydev <songlab.ru@gmail.com>
2022-10-13 06:35:26 +0400

    Added API tests

--- handlers/utils.js
+++ handlers/utils.js
@@ -1,16 +1,39 @@
-const { constants } = require("./constants");
+const {constants} = require("./constants");
 const axios = require("axios");
 const url = constants.BASEURL;
 
-const getUsers = gender => {
+const getUsers = (gender) => {
     return axios({
         method: 'get',
         url: `${url}/api/test/users?gender=${gender}`
     });
 };
 
+const getUser = (id) => {
+    return axios({
+        method: 'get',
+        url: `${url}/api/test/user/${id}`
+    });
+};
+
+const randomString = length => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+};
+
+const randomArrayValue = array => Math.floor(Math.random() * array.length);
+
 module.exports = {
-    getUsers
+    getUsers,
+    getUser,
+    randomString,
+    randomArrayValue
 };
 
 
--- tests/tests.test.js
+++ tests/tests.test.js
@@ -1,7 +1,109 @@
-const { getUsers } = require("../handlers/utils");
+const { getUsers, getUser, randomArrayValue } = require("../handlers/utils");
 
-test('Main case', () => {
-    return getUsers("male").then(response => {
-        expect(response.status).toBe(200);
-    })
+describe('/api/test/users?gender={gender}', () => {
+    it('should get valid structure of response', () => {
+        return getUsers('male').then(response => {
+            const expectedKeys = [
+                "success",
+                "errorCode",
+                "errorMessage",
+                "result"
+            ];
+            expect(response && typeof response === 'object').toBe(true);
+            expect(Object.keys(response.data)).toEqual(expect.arrayContaining(expectedKeys));
+            expect(Array.isArray(response.data.result)).toBe(true);
+        });
+    });
+
+    it('should get valid parameters of response', () => {
+        return getUsers('female').then(response => {
+            expect(response.status).toBe(200);
+            expect(response.statusText).toBe('OK');
+        });
+
+    });
+    it('should check success field', () => {
+        return getUsers('male').then(response => {
+            expect(response.data.success === true).toBe(true);
+            expect(typeof response.data.success === 'boolean').toBe(true);
+        });
+    });
+
+    it('should check errorCode field', () => {
+        return getUsers('female').then(response => {
+            expect(typeof response.data.errorCode === 'number').toEqual(true);
+            expect(response.data.errorCode).toEqual(0);
+        });
+    });
+
+    it('should check errorMessage field', () => {
+        return getUsers('female').then(response => {
+            expect(response.data.errorMessage).toBe(null);
+        });
+    });
+});
+
+describe('/api/test/user/{id}', () => {
+    const ids = getUsers('male').then(response => {
+        return response.data.result;
+    });
+    const id = randomArrayValue(ids);
+
+    it('should get valid structure of response', () => {
+        return getUser(id).then(response => {
+            const expectedKeys = [
+                "success",
+                "errorCode",
+                "errorMessage",
+                "result"
+            ];
+            expect(response && typeof response === 'object').toBe(true);
+            expect(Object.keys(response.data)).toEqual(expect.arrayContaining(expectedKeys));
+            expect(response.data.result && typeof response.data.result === 'object').toBe(true);
+        });
+    });
+
+    it('should get valid parameters of response', () => {
+        return getUser(id).then(response => {
+            expect(response.status).toBe(200);
+            expect(response.statusText).toBe('OK');
+        });
+
+    });
+
+    it('should check success field', () => {
+        return getUser(id).then(response => {
+            expect(response.data.success === true).toBe(true);
+            expect(typeof response.data.success === 'boolean').toBe(true);
+        });
+    });
+
+    it('should check errorCode field', () => {
+        return getUser(id).then(response => {
+            expect(typeof response.data.errorCode === 'number').toEqual(true);
+            expect(response.data.errorCode).toEqual(0);
+        });
+    });
+
+    it('should check errorMessage field', () => {
+        return getUser(id).then(response => {
+            expect(response.data.errorMessage).toBe(null);
+        });
+    });
+
+    it('should check type of fields', () => {
+        return getUser(id).then(response => {
+            expect(typeof response.data.result.id === 'string').toBe(true);
+            expect(typeof response.data.result.name === 'string').toBe(true);
+            expect(typeof response.data.result.gender === 'string').toBe(true);
+            expect(typeof response.data.result.age === 'string').toBe(true);
+            expect(typeof response.data.result.city === 'string').toBe(true);
+            expect(typeof response.data.result.registrationDate === 'string').toBe(true);
+        });
+    });
+    it('should check values of gender field', () => {
+        return getUser(id).then(response => {
+            expect(response.data.result.gender === 'Female' || response.data.result.gender === 'Male').toBe(true);
+        });
+    });
 });
